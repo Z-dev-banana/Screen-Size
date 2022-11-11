958480a8f3eca1cbfeff22e2ea9fb5cdfca483d8
Z-dev-banana <zachstrad3r@gmail.com>
2022-11-11 10:05:45 -0800

    finished script
    Single file screen size getter, converts cm to in and finds diagonal length by Pythagorean

---
+++ screensize.ps1
@@ -0,0 +1,10 @@
+﻿$Screens = Get-CimInstance -Namespace root\wmi -ClassName WmiMonitorBasicDisplayParams 
+
+$ScreenSizeD = @()
+
+foreach ($s in $Screens) {
+    $cmdiag = [Math]::Sqrt([Math]::Pow($s.MaxHorizontalImageSize,2) + [Math]::Pow($s.MaxVerticalImageSize,2))
+    $ScreenSizeD = $cmdiag * 0.3937007874
+    $screenName = $s.InstanceName
+    echo "Monitor: $screenName has diagonal screen size $ScreenSizeD inches"
+}
